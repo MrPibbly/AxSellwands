b7f8ab361fd0b0f71cf33766c94c400845b7e840
BenceX100 <birincsikbence@gmail.com>
2026-06-09 11:57:51 +0200

    1.14.2

--- src/main/java/com/artillexstudios/axsellwands/hooks/shop/ExcellentShopHook.java
+++ src/main/java/com/artillexstudios/axsellwands/hooks/shop/ExcellentShopHook.java
@@ -1,29 +1,41 @@
 package com.artillexstudios.axsellwands.hooks.shop;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import su.nightexpress.nexshop.ShopAPI;
-import su.nightexpress.nexshop.api.shop.type.TradeType;
-import su.nightexpress.nexshop.shop.virtual.impl.VirtualProduct;
+import org.jetbrains.annotations.NotNull;
+import su.nightexpress.excellentshop.ShopPlugin;
+import su.nightexpress.excellentshop.api.product.TradeType;
+import su.nightexpress.excellentshop.virtualshop.VirtualShopModule;
+import su.nightexpress.excellentshop.virtualshop.product.VirtualProduct;
 
 public class ExcellentShopHook implements PricesHook {
+    private VirtualShopModule module;
 
     @Override
     public void setup() {
-
+        ShopPlugin plugin = (ShopPlugin) Bukkit.getPluginManager().getPlugin("ExcellentShop");
+        if (plugin == null) return;
+        module = plugin.getModuleRegistry().byType(VirtualShopModule.class).orElse(null);
     }
 
     @Override
     public double getPrice(ItemStack it) {
-        VirtualProduct product = ShopAPI.getVirtualShop().getBestProductFor(it, TradeType.SELL);
-        if (product == null || !product.isSellable()) return 0;
-        return product.getPrice(TradeType.SELL) / product.getUnitAmount() * it.getAmount();
+        VirtualProduct product = module.getBestProductFor(copy(it), TradeType.SELL);
+        if (product == null) return -1.0D;
+        return product.getFinalPrice(TradeType.SELL, it.getAmount());
     }
 
     @Override
     public double getPrice(Player player, ItemStack it) {
-        VirtualProduct product = ShopAPI.getVirtualShop().getBestProductFor(it, TradeType.SELL);
-        if (product == null || !product.isSellable()) return 0;
-        return product.getFinalSellPrice(player) / product.getUnitAmount() * it.getAmount();
+        VirtualProduct product = module.getBestProductFor(copy(it), TradeType.SELL);
+        if (product == null) return -1.0D;
+        return product.getFinalPrice(TradeType.SELL, it.getAmount(), player);
+    }
+
+    private ItemStack copy(@NotNull ItemStack item) {
+        ItemStack copy = item.clone();
+        copy.setAmount(1);
+        return copy;
     }
 }
